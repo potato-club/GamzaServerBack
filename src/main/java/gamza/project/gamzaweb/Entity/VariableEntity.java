dfecf7c6a0dd5cd9bf5bc4ca1c009122c4d6ff6b
Teddysir <sdzx0719@naver.com>
2024-07-10 23:34:57 +0900

    add(entity): 환경변수 테이블 entity 추가

--- src/main/java/gamza/project/gamzaweb/Entity/VariableEntity.java
+++ src/main/java/gamza/project/gamzaweb/Entity/VariableEntity.java
@@ -1,4 +1,21 @@
 package gamza.project.gamzaweb.Entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class VariableEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String key;
+
 }
