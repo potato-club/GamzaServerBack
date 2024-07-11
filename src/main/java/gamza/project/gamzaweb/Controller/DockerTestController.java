7f12036a049defe4221ed2bb9ca22860e381694b
m2_macbook <dltkr717@gmail.com>
2024-07-11 23:51:43 +0900

    docker init

--- src/main/java/gamza/project/gamzaweb/Controller/DockerTestController.java
+++ src/main/java/gamza/project/gamzaweb/Controller/DockerTestController.java
@@ -1,7 +1,6 @@
 package gamza.project.gamzaweb.Controller;
 
 import com.github.dockerjava.api.DockerClient;
-import com.github.dockerjava.api.command.TagImageCmd;
 import com.github.dockerjava.api.model.Container;
 import com.github.dockerjava.core.DefaultDockerClientConfig;
 import com.github.dockerjava.core.DockerClientConfig;
@@ -9,14 +8,13 @@ import com.github.dockerjava.core.DockerClientImpl;
 import com.github.dockerjava.httpclient5.ApacheDockerHttpClient;
 import com.github.dockerjava.transport.DockerHttpClient;
 import gamza.project.gamzaweb.dctutil.DockerProvider;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.io.File;
 import java.io.IOException;
 import java.time.Duration;
 import java.util.List;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
 
 
 //https://github.com/docker-java/docker-java/blob/main/docs/getting_started.md
@@ -25,11 +23,11 @@ import java.util.concurrent.ExecutionException;
 @RequestMapping("/doc")
 public class DockerTestController {
 
-    DockerProvider provider = new DockerProvider();
-    DockerClient dockerClient = provider.getDockerClient();
-
     @GetMapping("/list")
     public String something() {
+        DockerProvider provider = new DockerProvider();
+        DockerClient dockerClient = provider.getDockerClient();
+
         List<Container> containers = dockerClient.listContainersCmd().exec();
         StringBuilder sb = new StringBuilder();
         sb.append("containers.size() : ").append(containers.size()).append("\n");
@@ -37,89 +35,8 @@ public class DockerTestController {
             sb.append(container.toString());
         }
 
+
+
         return sb.toString();
-    }
-
-    @GetMapping("/buildImage")
-    public String buildImage(
-            @RequestParam("name") String name,
-            @RequestParam("tag") String tag,
-            @RequestParam(value = "jasyptKey", required = false) String jasyptKey) {
-
-        File dockerfile = new File("/Users/kimseonghun/Desktop/docker/Dockerfile");
-
-        CompletableFuture<String> result = new CompletableFuture<>();
-
-        provider.buildImage(dockerfile, name, tag, jasyptKey, new DockerProvider.DockerProviderBuildCallback() {
-            @Override
-            public void getImageId(String imageId) {
-                result.complete(imageId);
-            }
-        });
-
-        try {
-            return result.get();
-        } catch (InterruptedException | ExecutionException e) {
-//            e.printStackTrace();
-            return e.getLocalizedMessage();
-        }
-    }
-
-//    @GetMapping("/buildImage")
-//    public String buildImage(
-//            @RequestParam("name") String name,
-//            @RequestParam("tag") String tag,
-//            @RequestParam(value = "jasyptKey", required = false) String jasyptKey) {
-//        CompletableFuture<String> result = new CompletableFuture<>();
-//        provider.buildImage(
-//                new File("/Users/kimseonghun/Desktop/docker/Dockerfile"),
-//                name,
-//                tag,
-//                new DockerProvider.DockerProviderBuildCallback() {
-//                    @Override
-//                    public void getImageId(String imageId) {
-//                        result.complete(imageId);
-//                    }
-//                }
-//        );
-//
-//        try {
-//            String imageId = result.get();
-//
-//            return imageId;
-//        } catch (InterruptedException e) {
-////            throw new RuntimeException(e);
-//            e.printStackTrace();
-//            return e.getLocalizedMessage();
-//        } catch (ExecutionException e) {
-////            throw new RuntimeException(e);
-//            e.printStackTrace();
-//            return e.getLocalizedMessage();
-//        }
-//    }
-
-    @GetMapping("/logs/{containerId}")
-    public List<String> getContainerLogs(@PathVariable("containerId") String containerId,
-                                         @RequestParam(value = "lines", defaultValue = "100") int lines) {
-        return provider.getContainerLogs(containerId, lines);
-    }
-
-//    @GetMapping("/update/nginx") // 피그마에 엔진엑스 conf 파일 수정 내용이 왜있지?
-//    public String updateNginxConfig(@RequestParam("containerId") String containerId,
-//                                    @RequestParam("port") String port,
-//                                    @RequestParam("cname") String cname) {
-//        return provider.updateNginxConfig(containerId, port, cname);
-//    }
-
-
-    @GetMapping("/create")
-    public String create() {
-        return provider.createContainer("test_name", "8082", "80", "1.0.0");
-    }
-    @GetMapping("/update")
-    public String update() {
-        //f19b857088ef7ebb0e7c6f144391add30ba0d457ed1ce6f4973eba152a837b94
-        //3c74c4202325
-        return provider.createContainer("test_name", "8082", "80", "1.0.0");
     }
 }
--- src/main/java/gamza/project/gamzaweb/dctutil/DockerProvider.java
+++ src/main/java/gamza/project/gamzaweb/dctutil/DockerProvider.java
@@ -1,36 +1,19 @@
 package gamza.project.gamzaweb.dctutil;
 
 import com.github.dockerjava.api.DockerClient;
-import com.github.dockerjava.api.command.BuildImageCmd;
-import com.github.dockerjava.api.command.BuildImageResultCallback;
-import com.github.dockerjava.api.command.CreateContainerResponse;
-import com.github.dockerjava.api.command.LogContainerCmd;
-import com.github.dockerjava.api.model.BuildResponseItem;
-import com.github.dockerjava.api.model.ExposedPort;
-import com.github.dockerjava.api.model.Frame;
-import com.github.dockerjava.api.model.Ports;
 import com.github.dockerjava.core.DefaultDockerClientConfig;
 import com.github.dockerjava.core.DockerClientConfig;
 import com.github.dockerjava.core.DockerClientImpl;
-import com.github.dockerjava.core.command.LogContainerResultCallback;
 import com.github.dockerjava.httpclient5.ApacheDockerHttpClient;
 import com.github.dockerjava.transport.DockerHttpClient;
-import jakarta.annotation.Nullable;
 
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.time.Duration;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.TimeUnit;
-
-import static com.github.dockerjava.api.model.HostConfig.newHostConfig;
 
 //https://github.com/docker-java/docker-java/blob/main/docs/getting_started.md
-//https://docs.docker.com/engine/api/v1.45/#tag/Image/operation/BuildPrune
+
 public class DockerProvider {
+
+
 
     public DockerClientConfig config = DefaultDockerClientConfig.createDefaultConfigBuilder().build();
 //    DockerClientConfig config = DefaultDockerClientConfig.createDefaultConfigBuilder()
@@ -53,154 +36,7 @@ public class DockerProvider {
 
 
     public DockerClient getDockerClient() {
+
         return DockerClientImpl.getInstance(config, httpClient);
     }
-
-
-    //examples start ---
-
-//    public String getJarPath() throws Exception {
-//        return new File(DockerProvider.class.getProtectionDomain().getCodeSource().getLocation()
-//                .toURI()).getPath();
-//    }
-
-
-    public void buildImage(File file, String name, @Nullable String tag, @Nullable String jasyptKey, DockerProviderBuildCallback callback) {
-        BuildImageCmd buildImageCmd = getDockerClient().buildImageCmd(file);
-
-        if (jasyptKey != null && !jasyptKey.isEmpty()) {
-            buildImageCmd.withBuildArg("JASYPT_KEY", jasyptKey);
-        }
-
-        buildImageCmd.exec(new BuildImageResultCallback() {
-            @Override
-            public void onNext(BuildResponseItem item) {
-                super.onNext(item);
-                System.out.println("onNext: " + item.getImageId());
-                if (item.getImageId() != null) {
-                    taggingImage(item.getImageId(), name, tag);
-                    callback.getImageId(item.getImageId());
-                }
-            }
-        });
-    }
-//    public void buildImage(File file, String name, @Nullable String tag, DockerProviderBuildCallback callback) {
-//        buildImage(file, new BuildImageResultCallback() {
-//            @Override
-//            public void onNext(BuildResponseItem item) {
-//                super.onNext(item);
-//                System.out.println("onNext: " + item.getImageId());
-//                if (item.getImageId() != null) {
-//                    taggingImage(item.getImageId(), name, tag);
-//                    callback.getImageId(item.getImageId());
-//                }
-//            }
-//        });
-//    }
-
-    public String createContainer(String name, String outerPort, String innerPort, String tag) {
-        ExposedPort tcpOuter = ExposedPort.tcp(Integer.parseInt(innerPort));
-        Ports portBindings = new Ports();
-        portBindings.bind(tcpOuter, Ports.Binding.bindPort(Integer.parseInt(outerPort)));
-
-        // create container from image
-        CreateContainerResponse container = getDockerClient().createContainerCmd(name)
-                .withExposedPorts(tcpOuter)
-                .withHostConfig(newHostConfig()
-                        .withPortBindings(portBindings))
-                .withImage(name + ":" + tag)
-                .exec();
-
-        // start the container
-        getDockerClient().startContainerCmd(container.getId()).exec();
-        return container.getId();
-    }
-
-    public void buildImage(File file, BuildImageResultCallback callback) {
-        System.out.println("buildImage : " + file.exists() + "/" + file.length());
-
-        BuildImageCmd image = getDockerClient().buildImageCmd(file);
-        image.exec(callback);
-    }
-
-    public void taggingImage(String imageId, String name, String tag) {
-        getDockerClient().tagImageCmd(imageId, name, tag).exec();
-    }
-
-    public List<String> getContainerLogs(String containerId, int lines) {
-        List<String> logs = new ArrayList<>();
-
-        try (LogContainerCmd logContainerCmd = getDockerClient().logContainerCmd(containerId)) {
-            logContainerCmd
-                    .withStdOut(true)
-                    .withStdErr(true)
-                    .withTail(lines)
-                    .exec(new LogContainerResultCallback() {
-                        @Override
-                        public void onNext(Frame frame) {
-                            logs.add(new String(frame.getPayload()));
-                        }
-                    }).awaitCompletion();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        return logs;
-    }
-
-
-
-//    public String updateNginxConfig(String containerId, String port, String cname) {
-//        try {
-//            String configContent = generateNginxConfig(port, cname);
-//            Path tempFile = Files.createTempFile("nginx", ".conf");
-//            Files.write(tempFile, configContent.getBytes());
-//
-//            getDockerClient().copyArchiveToContainerCmd(containerId)
-//                    .withHostResource(tempFile.toString())
-//                    .withRemotePath("/etc/nginx/nginx.conf")
-//                    .exec();
-//
-//            getDockerClient().restartContainerCmd(containerId).exec();
-//
-//            Files.delete(tempFile);
-//            return "Nginx config updated and container restarted.";
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//            return "Error updating Nginx config: " + e.getMessage();
-//        }
-//    }
-//
-//    private String generateNginxConfig(String port, String cname) {
-//        return """
-//                server {
-//                    listen %s;
-//                    server_name %s;
-//
-//                    location / {
-//                        proxy_pass http://localhost:8080;
-//                        proxy_set_header Host $host;
-//                        proxy_set_header X-Real-IP $remote_addr;
-//                        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-//                        proxy_set_header X-Forwarded-Proto $scheme;
-//                    }
-//                }
-//                """.formatted(port, cname);
-//    }
-
-
-    //example interface ---
-
-    public interface DockerProviderBuildCallback {
-        void getImageId(String imageId);
-    }
-
-    public interface innerDockerProviderBuildCallback {
-        void closeFunction();
-    }
-
-    //examples end---
-
-
 }
-
