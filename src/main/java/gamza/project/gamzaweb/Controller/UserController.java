46eed6802294983bce861b2291aa37e66ee7c622
Teddysir <sdzx0719@naver.com>
2024-07-15 00:24:18 +0900

    add(service): reissue API 1차개발 및 병합용 커밋

--- src/main/java/gamza/project/gamzaweb/Controller/UserController.java
+++ src/main/java/gamza/project/gamzaweb/Controller/UserController.java
@@ -3,6 +3,7 @@ package gamza.project.gamzaweb.Controller;
 import gamza.project.gamzaweb.Dto.User.RequestUserLoginDto;
 import gamza.project.gamzaweb.Dto.User.RequestUserSignUpDto;
 import gamza.project.gamzaweb.Service.Interface.UserService;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -25,6 +26,12 @@ public class UserController {
     public ResponseEntity<String> login(@RequestBody RequestUserLoginDto dto, HttpServletResponse response) {
         userService.login(dto, response);
         return ResponseEntity.ok().body("Success Login!");
+    }
+
+    @GetMapping("/reissue")
+    public ResponseEntity<String> reissue(HttpServletRequest request, HttpServletResponse response) {
+        userService.reissueToken(request, response);
+        return ResponseEntity.ok().body("Success reissue Token!");
     }
 
 }
