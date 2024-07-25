e2de6db7c722873ef5b454d2573534b8e8113b36
Teddysir <sdzx0719@naver.com>
2024-07-25 17:58:42 +0900

    merge 충돌 해결

--- src/main/java/gamza/project/gamzaweb/Controller/DockerTestController.java
+++ src/main/java/gamza/project/gamzaweb/Controller/DockerTestController.java
@@ -9,10 +9,7 @@ import com.github.dockerjava.core.DockerClientImpl;
 import com.github.dockerjava.httpclient5.ApacheDockerHttpClient;
 import com.github.dockerjava.transport.DockerHttpClient;
 import gamza.project.gamzaweb.dctutil.DockerProvider;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.File;
 import java.io.IOException;
@@ -43,40 +40,86 @@ public class DockerTestController {
         return sb.toString();
     }
 
-
     @GetMapping("/buildImage")
-    public String buildImage(@RequestParam String name, @RequestParam String tag) {
+    public String buildImage(
+            @RequestParam("name") String name,
+            @RequestParam("tag") String tag,
+            @RequestParam(value = "jasyptKey", required = false) String jasyptKey) {
+
+        File dockerfile = new File("/Users/kimseonghun/Desktop/docker/Dockerfile");
+
         CompletableFuture<String> result = new CompletableFuture<>();
-        provider.buildImage(
-                new File("/Users/isaacjang/Documents/test-nginx/Dockerfile"),
-                name,
-                tag,
-                new DockerProvider.DockerProviderBuildCallback() {
-                    @Override
-                    public void getImageId(String imageId) {
-                        result.complete(imageId);
-                    }
-                }
-        );
+
+        provider.buildImage(dockerfile, name, tag, jasyptKey, new DockerProvider.DockerProviderBuildCallback() {
+            @Override
+            public void getImageId(String imageId) {
+                result.complete(imageId);
+            }
+        });
 
         try {
-            String imageId = result.get();
-
-            return imageId;
-        } catch (InterruptedException e) {
-//            throw new RuntimeException(e);
-            e.printStackTrace();
-            return e.getLocalizedMessage();
-        } catch (ExecutionException e) {
-//            throw new RuntimeException(e);
-            e.printStackTrace();
+            return result.get();
+        } catch (InterruptedException | ExecutionException e) {
+//            e.printStackTrace();
             return e.getLocalizedMessage();
         }
     }
 
+//    @GetMapping("/buildImage")
+//    public String buildImage(
+//            @RequestParam("name") String name,
+//            @RequestParam("tag") String tag,
+//            @RequestParam(value = "jasyptKey", required = false) String jasyptKey) {
+//        CompletableFuture<String> result = new CompletableFuture<>();
+//        provider.buildImage(
+//                new File("/Users/kimseonghun/Desktop/docker/Dockerfile"),
+//                name,
+//                tag,
+//                new DockerProvider.DockerProviderBuildCallback() {
+//                    @Override
+//                    public void getImageId(String imageId) {
+//                        result.complete(imageId);
+//                    }
+//                }
+//        );
+//
+//        try {
+//            String imageId = result.get();
+//
+//            return imageId;
+//        } catch (InterruptedException e) {
+////            throw new RuntimeException(e);
+//            e.printStackTrace();
+//            return e.getLocalizedMessage();
+//        } catch (ExecutionException e) {
+////            throw new RuntimeException(e);
+//            e.printStackTrace();
+//            return e.getLocalizedMessage();
+//        }
+//    }
+
+    @GetMapping("/logs/{containerId}")
+    public List<String> getContainerLogs(@PathVariable("containerId") String containerId,
+                                         @RequestParam(value = "lines", defaultValue = "100") int lines) {
+        return provider.getContainerLogs(containerId, lines);
+    }
+
+//    @GetMapping("/update/nginx") // 피그마에 엔진엑스 conf 파일 수정 내용이 왜있지?
+//    public String updateNginxConfig(@RequestParam("containerId") String containerId,
+//                                    @RequestParam("port") String port,
+//                                    @RequestParam("cname") String cname) {
+//        return provider.updateNginxConfig(containerId, port, cname);
+//    }
+
 
     @GetMapping("/create")
     public String create() {
+        return provider.createContainer("test_name", "8082", "80", "1.0.0");
+    }
+    @GetMapping("/update")
+    public String update() {
+        //f19b857088ef7ebb0e7c6f144391add30ba0d457ed1ce6f4973eba152a837b94
+        //3c74c4202325
         return provider.createContainer("test_name", "8082", "80", "1.0.0");
     }
 }
