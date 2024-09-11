ea0d8f1d5307b857eebdfcdd7a30015819e4e76f
Teddysir <sdzx0719@naver.com>
2024-09-12 00:32:44 +0900

    hot fixed key error

--- src/main/java/gamza/project/gamzaweb/Dto/docker/RequestDockerImageDto.java
+++ src/main/java/gamza/project/gamzaweb/Dto/docker/RequestDockerImageDto.java
@@ -2,6 +2,7 @@ package gamza.project.gamzaweb.Dto.docker;
 
 import gamza.project.gamzaweb.Entity.ApplicationEntity;
 import gamza.project.gamzaweb.Entity.ImageEntity;
+import gamza.project.gamzaweb.Entity.VariableEntity;
 import lombok.Data;
 
 @Data
@@ -9,7 +10,7 @@ public class RequestDockerImageDto {
 
     private String name;
     private String tag;
-    private String key;
+    private VariableEntity key;
 //    private String dockerfilePath;
 
     public ApplicationEntity toEntity() {
