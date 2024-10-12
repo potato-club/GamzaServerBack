f5528b82251e7cc61ee33aa4fc8ca56eea9fefdc
kim jihyeon <jihyeon546@gmail.com>
2024-10-13 00:03:22 +0900

    fix: 마이페이지 response 값 수정

--- src/main/java/gamza/project/gamzaweb/Dto/project/ProjectListPerResponseDto.java
+++ src/main/java/gamza/project/gamzaweb/Dto/project/ProjectListPerResponseDto.java
@@ -1,17 +1,14 @@
 package gamza.project.gamzaweb.Dto.project;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
-
 import java.util.List;
 
 @Getter
-@NoArgsConstructor
-@AllArgsConstructor
 @Builder
 public class ProjectListPerResponseDto {
+
     private int size;
-    private List<ProjectPerResponseDto> contents;
+    private List<ProjectPerResponseDto> waitProjects;
+    private List<ProjectPerResponseDto> completeProjects;
 }
