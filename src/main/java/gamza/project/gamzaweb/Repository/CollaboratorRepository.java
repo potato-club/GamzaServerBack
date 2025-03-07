d69107b2458192d484d0f84fa4693735504c3a51
Teddysir <sdzx0719@naver.com>
2025-03-07 13:26:19 +0900

    fixed: 프로젝트 수정시 협력자 수정 로직 변경

--- src/main/java/gamza/project/gamzaweb/Repository/CollaboratorRepository.java
+++ src/main/java/gamza/project/gamzaweb/Repository/CollaboratorRepository.java
@@ -1,6 +1,7 @@
 package gamza.project.gamzaweb.Repository;
 
 import gamza.project.gamzaweb.Entity.CollaboratorEntity;
+import gamza.project.gamzaweb.Entity.ProjectEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,5 +11,7 @@ import java.util.Optional;
 public interface CollaboratorRepository extends JpaRepository<CollaboratorEntity, Long> {
 
     Optional<CollaboratorEntity> findByProjectIdAndUserId(Long project_id, Long user_id);
+
+    void deleteAllByProject(ProjectEntity project);
 
 }
