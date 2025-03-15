2668e384cc69d80b9f688fea12b49d524d82d9cc
Teddysir <sdzx0719@naver.com>
2025-03-15 16:34:09 +0900

    feat: 프로젝트 타입 중복 예외처리

--- src/main/java/gamza/project/gamzaweb/Validate/ProjectValidate.java
+++ src/main/java/gamza/project/gamzaweb/Validate/ProjectValidate.java
@@ -1,11 +1,15 @@
 package gamza.project.gamzaweb.Validate;
 
+import gamza.project.gamzaweb.Entity.PlatformEntity;
 import gamza.project.gamzaweb.Entity.ProjectEntity;
 import gamza.project.gamzaweb.Error.ErrorCode;
+import gamza.project.gamzaweb.Error.requestError.BadRequestException;
 import gamza.project.gamzaweb.Error.requestError.NotFoundException;
 import gamza.project.gamzaweb.Repository.ProjectRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @Component
@@ -16,5 +20,32 @@ public class ProjectValidate {
     public ProjectEntity validateProject(Long id) {
         return projectRepository.findById(id)
                 .orElseThrow(() -> new NotFoundException("Project not found", ErrorCode.NOT_FOUND_EXCEPTION));
+    }
+
+    public void platformChecker(PlatformEntity platform, String projectType) {
+        if (platform.getProjects().size() >= 3) {
+            throw new BadRequestException("하나의 플랫폼에는 4개 이상의 프로젝트를 가질 수 없습니다.", ErrorCode.INTERNAL_SERVER_EXCEPTION);
+        }
+
+        boolean hasFront = false;
+        boolean hasBack = false;
+
+        List<ProjectEntity> projectEntities = platform.getProjects();
+
+        for(ProjectEntity project : projectEntities) {
+            if(project.getProjectType().name().equals("FRONT")) {
+                hasFront = true;
+            }
+            if(project.getProjectType().name().equals("BACK")) {
+                hasBack = true;
+            }
+        }
+        if(hasFront) {
+            throw new BadRequestException("이미 FRONT프로젝트가 존재합니다 WAIT상태로 변경후 다시 시도해주세요.", ErrorCode.INTERNAL_SERVER_EXCEPTION);
+        }
+        if(hasBack) {
+            throw new BadRequestException("이미 BACK프로젝트가 존재합니다 WAIT상태로 변경후 다시 시도해주세요.", ErrorCode.INTERNAL_SERVER_EXCEPTION);
+        }
+
     }
 }
