9f85772abb7318db340de3d6c557d6e8200437d7
Teddysir <sdzx0719@naver.com>
2025-05-04 17:41:38 +0900

    fixed docker api plz real jjin mak

--- src/main/java/gamza/project/gamzaweb/dctutil/DockerDataStore.java
+++ src/main/java/gamza/project/gamzaweb/dctutil/DockerDataStore.java
@@ -22,8 +22,7 @@ public class DockerDataStore {
     }
 
     DockerClientConfig config = DefaultDockerClientConfig.createDefaultConfigBuilder()
-//            .withDockerHost("tcp://100.78.112.64:2375")  // 예: "tcp://192.168.0.10:2375"
-            .withDockerHost("tcp://0.0.0.0:2375")
+            .withDockerHost("tcp://100.78.112.64:2375")
             .build();
 
     DockerHttpClient httpClient = new ApacheDockerHttpClient.Builder()
