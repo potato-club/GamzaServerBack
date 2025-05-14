da3bc4b3acd1bff2bd5d3b5e2ee2ecb75dc73a98
Teddysir <sdzx0719@naver.com>
2025-05-14 19:25:56 +0900

    feat: parameter userChecker add

---
+++ src/main/java/gamza/project/gamzaweb/config/WebConfig.java
@@ -0,0 +1,22 @@
+package gamza.project.gamzaweb.config;
+
+
+import gamza.project.gamzaweb.validate.custom.AuthUserArgumentResolver;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebConfig implements WebMvcConfigurer {
+
+    private final AuthUserArgumentResolver authUserArgumentResolver;
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(authUserArgumentResolver);
+    }
+}
